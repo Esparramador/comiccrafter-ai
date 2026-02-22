7076b80519fa06f683c4f10d9619b3aaea688114
Joan Sadia gil <sadiagiljoan@gmail.com>
2026-02-22 03:03:27 +0000

    File changes

--- src/pages/CreateComic.jsx
+++ src/pages/CreateComic.jsx
@@ -251,7 +251,6 @@ ALL dialogues and text on panels MUST be in ${languageName}. visual_prompt must 
       URL.revokeObjectURL(zipUrl);
     } catch (e) { /* ZIP optional, don't block navigation */ }
 
-    if (draftId) await base44.entities.Draft.delete(draftId);
     navigate(createPageUrl("ComicViewer") + `?id=${comic.id}`);
   };
 
