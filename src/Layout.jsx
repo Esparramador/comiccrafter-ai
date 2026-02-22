f9fe5425fb3206f581646332f58c244510172082
Joan Sadia gil <sadiagiljoan@gmail.com>
2026-02-22 02:36:04 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { createPageUrl } from "./utils";
 import { 
-  Sparkles, BookOpen, PlusCircle, Menu, X, Zap, Users, Film
+  Sparkles, BookOpen, PlusCircle, Menu, X, Zap, Users, Film, FileText
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
