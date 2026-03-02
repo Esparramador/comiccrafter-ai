8064f79a574130d417556dfbbf0d4a37c7510c23
Esparramador <sadiagiljoan@gmail.com>
2026-03-02 02:30:19 +0100

    Remove Base44 junk files, keep only Comic Crafter project

--- src/Layout.jsx
+++
@@ -1,9 +0,0 @@
-import MobileLayout from "./components/mobile/MobileLayout";
-
-export default function Layout({ children, currentPageName }) {
-  return (
-    <MobileLayout currentPageName={currentPageName}>
-      {children}
-    </MobileLayout>
-  );
-}
